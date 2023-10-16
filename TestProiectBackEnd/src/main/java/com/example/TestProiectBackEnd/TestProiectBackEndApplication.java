ee56b1cad8c1f3aca87b4c82d6c82edf40707932
l.malina <malinaluc@yahoo.com>
2023-10-16 17:58:19 +0300

    final lab3

---
+++ TestProiectBackEnd/src/main/java/com/example/TestProiectBackEnd/TestProiectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestProiectBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProiectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProiectBackEndApplication.class, args);
+	}
+
+}
---
+++ TestProiectBackEnd/src/test/java/com/example/TestProiectBackEnd/TestProiectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestProiectBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProiectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
